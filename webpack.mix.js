11abe37ef10a0c63e3c09ba8e2201c158f95d863
John Morton <john@johnfmorton.com>
2018-05-02 10:59:14 -0400

    Initial Commit

---
+++ webpack.mix.js
@@ -0,0 +1,104 @@
+let mix = require('laravel-mix');
+let tailwindcss = require("tailwindcss");
+let glob = require("glob-all");
+let PurgecssPlugin = require("purgecss-webpack-plugin");
+
+require('laravel-mix-purgecss');
+require('laravel-mix-criticalcss');
+
+/**
+ * Custom PurgeCSS Extractor
+ * https://github.com/FullHuman/purgecss
+ * https://github.com/FullHuman/purgecss-webpack-plugin
+ */
+class TailwindExtractor {
+    static extract(content) {
+        return content.match(/[A-z0-9-:\/]+/g) || [];
+    }
+}
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+mix.postCss("./src/styles.css", "web/static/css", [tailwindcss("./tailwind.js")])
+    .purgeCss({
+        enabled: mix.inProduction(),
+        // Your custom globs are merged with the default globs. If you need to fully replace
+        // the globs, use the underlying `paths` option instead.
+        globs: [
+            path.join(__dirname, 'templates/**/*.twig'),
+        ],
+
+        extensions: ['html', 'twig', 'js', 'php', 'vue'],
+
+        // Other options are passed through to Purgecss
+        // whitelistPatterns: [/language/, /hljs/],
+    })
+    .criticalCss({
+        enabled: mix.inProduction(),
+        paths: {
+            base: 'http://jmx2.jfm/',
+            templates: './templates/criticalCss/'
+        },
+        urls: [
+            { url: '', template: 'home' },
+            { url: 'about', template: 'about' },
+            // { url: '/', template: 'index-critical.min.css' },
+            // { src: 'http://jmx2.jfm/', dest: 'index_critical.min.css' },
+            // { src: 'about', dest: 'about_critical.min.css' }
+        ],
+        options: {
+            minify: true,
+        },
+    });
+
+
+mix.js('src/app.js', 'web/static/js')
+
+//     .sass('src/app.scss', 'web/static/');
+
+// Full API
+// mix.js(src, output);
+// mix.react(src, output); <-- Identical to mix.js(), but registers React Babel compilation.
+// mix.preact(src, output); <-- Identical to mix.js(), but registers Preact compilation.
+// mix.coffee(src, output); <-- Identical to mix.js(), but registers CoffeeScript compilation.
+// mix.ts(src, output); <-- TypeScript support. Requires tsconfig.json to exist in the same folder as webpack.mix.js
+// mix.extract(vendorLibs);
+// mix.sass(src, output);
+// mix.standaloneSass('src', output); <-- Faster, but isolated from Webpack.
+// mix.fastSass('src', output); <-- Alias for mix.standaloneSass().
+// mix.less(src, output);
+// mix.stylus(src, output);
+// mix.postCss(src, output, [require('postcss-some-plugin')()]);
+// mix.browserSync('my-site.test');
+// mix.combine(files, destination);
+// mix.babel(files, destination); <-- Identical to mix.combine(), but also includes Babel compilation.
+// mix.copy(from, to);
+// mix.copyDirectory(fromDir, toDir);
+// mix.minify(file);
+// mix.sourceMaps(); // Enable sourcemaps
+// mix.version(); // Enable versioning.
+// mix.disableNotifications();
+// mix.setPublicPath('path/to/public');
+// mix.setResourceRoot('prefix/for/resource/locators');
+// mix.autoload({}); <-- Will be passed to Webpack's ProvidePlugin.
+// mix.webpackConfig({}); <-- Override webpack.config.js, without editing the file directly.
+// mix.babelConfig({}); <-- Merge extra Babel configuration (plugins, etc.) with Mix's default.
+// mix.then(function () {}) <-- Will be triggered each time Webpack finishes building.
+// mix.extend(name, handler) <-- Extend Mix's API with your own components.
+// mix.options({
+//   extractVueStyles: false, // Extract .vue component styling to file, rather than inline.
+//   globalVueStyles: file, // Variables file to be imported in every component.
+//   processCssUrls: true, // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
+//   purifyCss: false, // Remove unused CSS selectors.
+//   uglify: {}, // Uglify-specific options. https://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+//   postCss: [] // Post-CSS options: https://github.com/postcss/postcss/blob/master/docs/plugins.md
+// });
